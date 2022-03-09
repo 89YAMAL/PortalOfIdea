fedb3092bdb07f52f9ab765f3d386cc4298feb3e
89YAMAL <98641659+89YAMAL@users.noreply.github.com>
2022-03-09 22:32:06 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.menu-burger').on('click', function() {
+    $('.menu').slideToggle(200, function() {
+      if($(this).css('display') === 'none') {
+        $(this).removeAttr('style');
+      }
+    })
+  })
+})
+
+$('body').bind('touchstart', function () {});
